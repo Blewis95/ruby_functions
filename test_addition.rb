b755afda4b1bac7d32f4c3f0674c1142bad2edcf
Brian Lewis <blewis8958@gmail.com>
2017-01-25 11:44:49 -0500

    added more tests and added code into the add functions

--- test_addition.rb
+++ test_addition.rb
@@ -5,4 +5,16 @@ class TestAddition < Minitest::Test
 	def test_1_plus_1
 		assert_equal(2,add(1,1))
 	end
+
+	def test_2_plus_3
+		assert_equal(5,add(2,3))
+	end
+
+	def test_2_point_5_plus_7
+		assert_equal(9.5,add(2.5,7))
+	end
+
+	def test_string_addition
+		assert_equal("Hello World",add("Hello ", "World"))
+	end
 end
