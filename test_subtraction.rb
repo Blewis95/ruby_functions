f6a6fa843134c813e621187e42f86572f9ecddc1
Brian Lewis <blewis8958@gmail.com>
2017-01-25 12:20:31 -0500

    Created a subtraction test file and code file with test cases and working code

---
+++ test_subtraction.rb
@@ -0,0 +1,25 @@
+require "minitest/autorun"
+require_relative "subtraction.rb"
+
+class TestAddition < Minitest::Test
+	def test_1_minus_1
+		assert_equal(0,subtract(1,1))
+	end
+
+	def test_3_minus_2
+		assert_equal(1,subtract(3,2))
+	end
+
+	def test_7_minus_2_point_5
+		assert_equal(4.5,subtract(7,2.5))
+	end
+
+	# => FAILED
+	# def test_string_subtraction
+	# 	assert_equal("Hell",subtract("Hello ", "o "))
+	# end
+
+	def test_negative_numbers
+		assert_equal(14,subtract(10,-4))
+	end
+end
