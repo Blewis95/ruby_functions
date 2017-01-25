7e480fc51ad66c0d2ecd87eb1d204ba1e195ed78
Brian Lewis <blewis8958@gmail.com>
2017-01-25 13:01:03 -0500

    Spell checking and fixing

--- subtraction.rb
+++ subtraction.rb
@@ -1,3 +1,7 @@
 def subtract(num1, num2)
-	difference = num1 - num2
+	if num1.class != Fixnum || num2.class != Fixnum
+		print "Please enter 2 numbers and try again"
+	else
+		difference = num1 - num2
+	end
 end
